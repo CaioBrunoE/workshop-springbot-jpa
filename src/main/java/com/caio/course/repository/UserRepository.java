dbae8e270eb3393ec61adfd170d4561e2c1ce2f3
Caio Bruno <caiobruno90@yahoo.com>
2023-10-09 16:20:26 -0300

    Service layer, component registration

--- src/main/java/com/caio/course/repository/UserRepository.java
+++ src/main/java/com/caio/course/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.caio.course.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.caio.course.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	
---
+++ src/main/java/com/caio/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.caio.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.caio.course.entities.User;
+import com.caio.course.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findALL(){
+		return repository.findAll();
+	} 
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
