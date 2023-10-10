56d793da3c16b0fedf465c1561fb4c12b18cbf7c
Caio Bruno <caiobruno90@yahoo.com>
2023-10-10 10:58:06 -0300

    OrderStatus enum

---
+++ src/main/java/com/caio/course/entities/enums/OrderStatus.java
@@ -0,0 +1,34 @@
+package com.caio.course.entities.enums;
+
+import java.util.Iterator;
+
+public enum OrderStatus {
+    
+	WAITING_PAYMENT(1),
+	PAID(2),
+	SHIPED(3),
+	DELIVERED(4),
+	CANCELED(5);
+	
+	private int code ;
+	
+	private OrderStatus(int code ) {
+		this.code=code;
+	}
+	
+	
+	public int getCode() {
+		return code ;
+		
+	}
+	
+	public static OrderStatus  valueOf(int code ) {
+		for (OrderStatus value : OrderStatus.values()) {
+			if(value.getCode() == code ) {
+				return value;
+			}
+			
+		}
+		 throw new IllegalArgumentException("Invalid order Satatus code");
+	}
+}
