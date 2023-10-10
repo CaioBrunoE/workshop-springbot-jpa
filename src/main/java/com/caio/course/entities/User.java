3615c0a0fc3e1b7f6d1ea6374c4ee1d9a04543c5
Caio Bruno <caiobruno90@yahoo.com>
2023-10-10 09:22:54 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/caio/course/entities/User.java
+++ src/main/java/com/caio/course/entities/User.java
@@ -1,12 +1,16 @@
 package com.caio.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,13 +22,20 @@ public class User implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column
 	private String name;
 	
+	@Column
 	private String email;
 	
+	@Column
 	private String phone;
 	
+	@Column
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>() ;
 	
 	public User () {
 		
@@ -78,7 +89,11 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+    
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -95,6 +110,8 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
+	
 	
 	
 	
