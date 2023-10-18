cf4ca4c9297fa07a610de4b6fec3cd064104b5c2
Caio Bruno <caiobruno90@yahoo.com>
2023-10-18 09:54:34 -0300

    User update

--- src/main/java/com/caio/course/services/UserService.java
+++ src/main/java/com/caio/course/services/UserService.java
@@ -2,7 +2,6 @@ package com.caio.course.services;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -33,5 +32,16 @@ public class UserService {
 		repository.deleteById(id);
 	}
 	
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity , obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 	
 }
