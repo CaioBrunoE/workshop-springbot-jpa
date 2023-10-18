06a7e5d06b7ef4ac68d21abd68ee4d868b6ace9e
Caio Bruno <caiobruno90@yahoo.com>
2023-10-18 10:43:34 -0300

    Exception handling - delete

--- src/main/java/com/caio/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/caio/course/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.caio.course.services.exceptions.DatabaseException;
 import com.caio.course.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,4 +23,14 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(Instant.now(), status.value(), error , e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> Database(DatabaseException e, HttpServletRequest request){
+		
+		String error =  "Data base ERROR";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error , e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
 }
--- src/main/java/com/caio/course/services/UserService.java
+++ src/main/java/com/caio/course/services/UserService.java
@@ -2,11 +2,15 @@ package com.caio.course.services;
 
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.caio.course.entities.User;
 import com.caio.course.repository.UserRepository;
+import com.caio.course.services.exceptions.DatabaseException;
 import com.caio.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -14,28 +18,35 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findALL(){
+
+	public List<User> findALL() {
 		return repository.findAll();
-	} 
-	
+	}
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
-		
+
 	}
-	
+
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
-	
+
 	public User update(Long id, User obj) {
 		User entity = repository.getReferenceById(id);
-		updateData(entity , obj);
+		updateData(entity, obj);
 		return repository.save(entity);
 	}
 
@@ -44,5 +55,5 @@ public class UserService {
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
 	}
-	
+
 }
